81900f48431e16011ae4d53e2a72556494e1e3d8
khhuisman <k.h.huisman@tudelft.nl>
2023-04-11 15:54:47 +0200

    .py files are in a folder

---
+++ github_tests/test.py
@@ -0,0 +1,16 @@
+print("Hello World")
+
+import numpy
+
+def func_circum(r):
+	'''
+	Input: 
+	r = radius 
+	Ouput: 
+	Circumference of a circle with radius r
+	'''
+	return 2*np.pi*r
+
+def func_area(r):
+	'''Calculate surface area of a circle with radius r'''
+	return np.pi*(r**2)
