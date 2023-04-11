0ea7da20c5c4be2597ca785677a8702b215e862f
khhuisman <k.h.huisman@tudelft.nl>
2023-04-11 15:48:42 +0200

    test imported into script.py, functions called for r = 1

--- script.py
+++ script.py
@@ -1,2 +1,7 @@
 print("There were bells, on a hill.")
 print("And I never heard them ringing, no I never heard them at all")
+
+import test
+
+r = 1
+print('radius is',r, 'circumference = ',test.func_circum(r), 'and', 'surface area is ', test.func_area(r))
